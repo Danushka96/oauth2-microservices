c73580c18a1c143352305ac2d5180276224b6270
danushka96 <danushkaherath96@gmail.com>
2021-05-04 14:28:44 +0530

    added central config server

---
+++ central-configuration/src/test/java/com/easymedic/centralconfiguration/CentralConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymedic.centralconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
